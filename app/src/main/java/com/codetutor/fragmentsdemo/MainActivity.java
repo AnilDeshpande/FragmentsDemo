3c94bc31481a9bd2d71369d3cc5483e1e61bfa10
Anil Deshpande <anildesh82@gmail.com>
2017-10-25 12:44:58 +0530

    dynamic fragment implementation done

--- app/src/main/java/com/codetutor/fragmentsdemo/MainActivity.java
+++ app/src/main/java/com/codetutor/fragmentsdemo/MainActivity.java
@@ -1,5 +1,9 @@
 package com.codetutor.fragmentsdemo;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addFragment();
+    }
+
+    private void addFragment(){
+        Fragment fragment=new SampleFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainer,fragment,"demofragment");
+        //fragmentTransaction.addToBackStack("fragmentStack1");
+        fragmentTransaction.commit();
+
     }
 }
