ad541474cadf3f1ca0b8fc8f945c3fd594d5312a
Anil Deshpande <anildesh82@gmail.com>
2017-10-25 12:34:09 +0530

    First Commit - Static Fragment

---
+++ app/src/main/java/com/codetutor/fragmentsdemo/SampleFragment.java
@@ -0,0 +1,21 @@
+package com.codetutor.fragmentsdemo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by anildeshpande on 10/25/17.
+ */
+
+public class SampleFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sample,container,false);
+    }
+}
