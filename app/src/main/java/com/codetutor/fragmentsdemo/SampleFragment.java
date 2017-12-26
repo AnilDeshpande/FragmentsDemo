b27b69cb8bdca1b47e4a827cc7b89ea33f87bae1
Anil Deshpande <anildesh82@gmail.com>
2017-12-26 22:25:43 +0530

    first commit

--- app/src/main/java/com/codetutor/fragmentsdemo/SampleFragment.java
+++ app/src/main/java/com/codetutor/fragmentsdemo/SampleFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by anildeshpande on 10/25/17.
@@ -15,9 +17,22 @@ import android.view.ViewGroup;
 
 public class SampleFragment extends Fragment{
 
+    View rootView;
+
+    ListView listViewCountries;
+    ArrayAdapter<String> countryNamesAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sample,container,false);
+        rootView = inflater.inflate(R.layout.fragment_sample,container,false);
+        initUI();
+        return rootView;
+    }
+
+    private void initUI(){
+        listViewCountries = (ListView)rootView.findViewById(R.id.listViewCountries);
+        countryNamesAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.countries));
+        listViewCountries.setAdapter(countryNamesAdapter);
     }
 }
